e469abe5bb2b3de48d2e5ad77c8ffbd129a92ab2
Loki0267 <s2021045@cco.kanagawa-it.ac.jp>
2022-09-06 21:00:45 +0900

    画面遷移の練習

---
+++ NaviAndTab/NaviAndTab/Friends.swift
@@ -0,0 +1,20 @@
+//
+//  Friends.swift
+//  NaviAndTab
+//
+//  Created by kiyolab02 on 2022/09/06.
+//
+
+import SwiftUI
+
+struct Friends: View {
+    var body: some View {
+        Text("Friends")
+    }
+}
+
+struct Friends_Previews: PreviewProvider {
+    static var previews: some View {
+        Friends()
+    }
+}
---
+++ NaviAndTab/NaviAndTab/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  NaviAndTab
+//
+//  Created by kiyolab02 on 2022/09/06.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text("Home")
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
